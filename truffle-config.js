925d6678fa9da29dd1481b7fdb93fc17643c751b
CodeStorm <31198108+PrajwalMore@users.noreply.github.com>
2021-07-02 22:57:57 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,1 +1,43 @@
-//to be updated.
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.rinkeby_URL)
+      },
+      network_id: '4',
+      skipDryRun: false,
+    },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MAINNET_MNEMONIC, process.env.MAINNET_RPC_URL)
+      },
+      network_id: '1',
+      skipDryRun: true,
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.ropsten_URL)
+      },
+      network_id: '3',
+      skipDryRun: true,
+    },ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "5777"
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.6.6',
+    },
+  },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ]
+}
