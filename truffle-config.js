3840cb84bff5ac25529a35692f21ee9367a89516
CodeStorm <31198108+PrajwalMore@users.noreply.github.com>
2021-07-07 20:44:00 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,19 @@ module.exports = {
       },
       network_id: '3',
       skipDryRun: true,
+    }, Bsctestnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },ganache: {
       host: "localhost",
       port: 7545,
